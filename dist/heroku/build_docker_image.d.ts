6eb2dbcdd58b1abdb7deb6c90c371eb42c8a3dd5
TheFSilverSP <80601861+TheFSilverSP@users.noreply.github.com>
2021-06-19 03:04:31 +0200

    Update build_docker_image.d.ts

--- dist/heroku/build_docker_image.d.ts
+++ dist/heroku/build_docker_image.d.ts
@@ -1,7 +1,9 @@
-export declare const buildDockerImage: ({ dockerfileName, dockerOptions, herokuAppName, cwd, processType, }: {
+export declare const buildDockerImage: ({ dockerfileName, dockerOptions, herokuAppName, presearchPrivateKey, presearchPublicKey, cwd, processType, }: {
     dockerfileName: string;
     dockerOptions: string;
     herokuAppName: string;
+    presearchPrivateKey: string;
+    presearchPublicKey: string;
     cwd: string;
     processType: string;
 }) => Promise<boolean>;
